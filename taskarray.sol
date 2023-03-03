e3be2b2f03356acba63034b64a2565d87bf8ef8f
Haris <harismuneer8@gmail.com>
2023-03-03 16:41:55 +0500

    make the array external

--- taskarray.sol
+++ taskarray.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.7;
 
 contract arraytask{
 
+    uint[]  arrayshow;
 
    function arraySequence(uint256 number) public returns(uint256[] memory) {
-       uint[] memory arrayshow;
+       
         arrayshow.push(number);
         return arrayshow;
     
