ef3aaf543f2fbaa7ff88e6ac03cebd2de9b4510b
Haris <harismuneer8@gmail.com>
2023-04-14 12:04:25 +0500

    updated

--- erc721.sol
+++ erc721.sol
@@ -1,8 +1,22 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.7;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MyToken is ERC721 {
-    constructor() ERC721("MyToken", "MTK") {}
-}
+
+contract MyNFT is ERC721URIStorage, Ownable{
+
+    constructor() ERC721("SharksPk", "SK"){}
+
+    function mint(address to,uint tokenId,string calldata uri) external onlyOwner{
+        _mint(to,tokenId);
+        _setTokenURI(tokenId, uri);
+        
+    }
+
+
+
+    }
+
+        
